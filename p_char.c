35265a923e7a04dfc31e699fc361edf138942352
Amanda Gifford <amandagifford@Amandas-MacBook-Pro.local>
2018-08-12 16:45:51 -0700

    finally fixed ^@

--- p_char.c
+++ p_char.c
@@ -1,5 +1,20 @@
 #include "ft_printf.h"
 
+void	null_print(t_components *m)
+{
+	char	t[1];
+	
+	t[0] = '\0';
+	if (m->arg.c == '\0')
+	{
+		ft_putstr(m->buff);
+		write(1, &t, 1);
+		m->buff = ft_strnew(0);
+	}	
+	else
+		print_char(m, m->arg.c, 1);
+}
+
 void	p_char(t_components *m)
 {
 	m->arg.c = (char)va_arg(m->args, int);
@@ -7,15 +22,16 @@ void	p_char(t_components *m)
 	m->spaces = m->width - m->len;
 	if (CHECK_MINUS(m->flags))
 	{
-		print_char(m, m->arg.c, 1);
+		null_print(m);
 		if (m->spaces > 0)
 			print_char(m, ' ', m->spaces);
+	
 	}
 	else
 	{
 		if (m->spaces > 0)
 			print_char(m, ' ', m->spaces);
-		print_char(m, m->arg.c, 1);
+		null_print(m);		
 	}
 }
 
